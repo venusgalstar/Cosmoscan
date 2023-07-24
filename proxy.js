80345ef0899ac8d20716829be3c3731e4beabc81
= <=>
2023-07-24 14:32:45 +0200

    update

--- proxy.js
+++ proxy.js
@@ -1,84 +1,41 @@
-//  Install npm dependencies first
-//  npm init
-//  npm install --save url@0.10.3
-//  npm install --save http-proxy@1.11.1
 
-var httpProxy = require("http-proxy");
-var http = require("http");
-var url = require("url");
-var net = require('net');
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var axios = require('axios');
+const router = express.Router();
+var request = require('request');
 
-var server = http.createServer(function (req, res) {
-  var urlObj = url.parse(req.url);
-  urlObj.host = "www.mintscan.io";
-  urlObj.protocol = "https:";
-  var target = urlObj.protocol + "//" + urlObj.host;
 
-  console.log("Proxy HTTP request for:", target);
+var app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
 
-  var proxy = httpProxy.createProxyServer({});
-  proxy.on("error", function (err, req, res) {
-    console.log("proxy error", err);
-    res.end();
-  });
+async function getReponse(req, res) {
 
-  proxy.web(req, res, {target: target});
-}).listen(8080);  //this is the port your clients will connect to
+    const protocol = "https";
+    const host = req.hostname == "localhost" ? "www.mintscan.io" : req.hostname;
+    const url = req.originalUrl;
 
-var regex_hostport = /^([^:]+)(:([0-9]+))?$/;
+    const fullUrl = `${protocol}://${host}${url}`;
 
-// var getHostPortFromString = function (hostString, defaultPort) {
-//   var host = hostString;
-//   var port = defaultPort;
+    try{
+        console.log(req.hostname + req.originalUrl);
+        console.log(fullUrl);
+        // var respnose = await axios.get(fullUrl);
+    
+        req.pipe(request(fullUrl)).pipe(res);
+    
+        // return respnose;
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+app.get('*', async(req, res) => {
+    var response = await getReponse(req, res);
+    // console.log(res.get('Content-Type'));
+    // res.send(response.data);
+});
 
-//   var result = regex_hostport.exec(hostString);
-//   if (result != null) {
-//     host = result[1];
-//     if (result[2] != null) {
-//       port = result[3];
-//     }
-//   }
-
-//   return ( [host, port] );
-// };
-
-// server.addListener('connect', function (req, socket, bodyhead) {
-//   var hostPort = getHostPortFromString(req.url, 443);
-//   var hostDomain = hostPort[0];
-//   var port = parseInt(hostPort[1]);
-//   console.log("Proxying HTTPS request for:", hostDomain, port);
-//   console.log("hahah");
-  
-//   var proxySocket = new net.Socket();
-//   proxySocket.connect(port, hostDomain, function () {
-//       proxySocket.write(bodyhead);
-//       socket.write("HTTP/" + req.httpVersion + " 200 Connection established\r\n\r\n");
-//     }
-//   );
-
-//   proxySocket.on('data', function (chunk) {
-//     socket.write(chunk);
-//   });
-
-//   proxySocket.on('end', function () {
-//     socket.end();
-//   });
-
-//   proxySocket.on('error', function () {
-//     socket.write("HTTP/" + req.httpVersion + " 500 Connection error\r\n\r\n");
-//     socket.end();
-//   });
-
-//   socket.on('data', function (chunk) {
-//     proxySocket.write(chunk);
-//   });
-
-//   socket.on('end', function () {
-//     proxySocket.end();
-//   });
-
-//   socket.on('error', function () {
-//     proxySocket.end();
-//   });
-
-// });
+app.listen(8080);
